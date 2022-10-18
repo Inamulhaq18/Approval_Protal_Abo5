e53a5e96c3c36e902bbd67a28f4528531cabc2a8
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-10-18 13:55:04 +0300

    Update rembgprocess.py

--- pages/rembgprocess.py
+++ pages/rembgprocess.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import requests
 import base64
+import base64
+from PIL import Image
+from io import BytesIO
+
 
 with open("best-skincare-products-1656081764.jpg", "rb") as image_file:
     encoded_string = base64.b64encode(image_file.read())
@@ -10,5 +14,10 @@ encoded_string=str(encoded_string).replace("b'","")
 r = requests.post(url='https://hf.space/embed/eugenesiow/remove-bg/+/api/predict/', json={"data": ["data:image/jpeg;base64,"+encoded_string]})
 
 
-st.write(r.json())
-
+st.write(r.json()["data"])
+'''
+f = open('C:\\Users\\pc\\Desktop\\base64.txt','r')
+data = f.read()
+im = Image.open(BytesIO(base64.b64decode(data)))
+im.save('C:\\Users\\pc\\Desktop\\image.png', 'PNG')
+'''
