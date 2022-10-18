fa566ff1328b3eeaa8c5f8c8cbc6f23a8caeac5c
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-10-18 15:25:13 +0300

    Update rembgprocess.py

--- pages/rembgprocess.py
+++ pages/rembgprocess.py
@@ -11,7 +11,12 @@ links=st.text_input("Link")
 
 
 a=base64.b64encode(requests.get(links).content)
-st.write(a)
+st.write("Converstion to B64 done!")
+imgs=str(a).replace("b'","")
+r = requests.post(url='https://hf.space/embed/eugenesiow/remove-bg/+/api/predict/', json={"data": ["data:image/jpeg;base64,"+imgs]})
+st.write(r.json())
+opimg=str(r.json()["data"][0]).replace("data:image/png;base64,","")
+st.img(opimg)
 
 '''def get_as_base64(url):
     return base64.b64encode(requests.get(url).content)
@@ -21,9 +26,7 @@ def rembgapi(urls):
     imgs=str(imgs).replace("b'","")
     st.write(imgs)
     
-    r = requests.post(url='https://hf.space/embed/eugenesiow/remove-bg/+/api/predict/', json={"data": ["data:image/jpeg;base64,"+imgs]})
-    st.write(r.json())
-    opimg=str(r.json()["data"][0]).replace("data:image/png;base64,","")
+
     im = Image.open(BytesIO(base64.b64decode(opimg)))
     st.image(im)
     '''
