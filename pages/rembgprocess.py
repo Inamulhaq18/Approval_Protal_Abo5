b1e1b0f96ea21778ad556610805436d79a8279e0
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-10-18 14:18:45 +0300

    Update rembgprocess.py

--- pages/rembgprocess.py
+++ pages/rembgprocess.py
@@ -13,22 +13,12 @@ def get_as_base64(url):
 
 def rembgapi(urls):
     imgs=get_as_base64(urls)
-    st.write(imgs)
+    imgs=str(imgs).replace("b'","")
+    r = requests.post(url='https://hf.space/embed/eugenesiow/remove-bg/+/api/predict/', json={"data": ["data:image/jpeg;base64,"+imgs]})
+    opimg=str(r.json()["data"][0]).replace("data:image/png;base64,","")
+    im = Image.open(BytesIO(base64.b64decode(opimg)))
+    st.image(im)
+    
     
 links=st.text_input("Link")
-rembgapi(links)
-
-'''
-with open("best-skincare-products-1656081764.jpg", "rb") as image_file:
-    encoded_string = base64.b64encode(image_file.read())
-encoded_string=str(encoded_string).replace("b'","")
-
-
-r = requests.post(url='https://hf.space/embed/eugenesiow/remove-bg/+/api/predict/', json={"data": ["data:image/jpeg;base64,"+encoded_string]})
-
-
-opimg=str(r.json()["data"][0]).replace("data:image/png;base64,","")
-
-im = Image.open(BytesIO(base64.b64decode(opimg)))
-st.image(im)
-'''
+encoded_string=rembgapi(links)
