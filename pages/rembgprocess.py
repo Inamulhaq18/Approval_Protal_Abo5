68ff36e248183ac84338fd1cac963e1074ea1b6b
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-10-18 14:22:38 +0300

    Update rembgprocess.py

--- pages/rembgprocess.py
+++ pages/rembgprocess.py
@@ -15,6 +15,7 @@ def rembgapi(urls):
     imgs=get_as_base64(urls)
     imgs=str(imgs).replace("b'","")
     r = requests.post(url='https://hf.space/embed/eugenesiow/remove-bg/+/api/predict/', json={"data": ["data:image/jpeg;base64,"+imgs]})
+    st.write(r.json())
     opimg=str(r.json()["data"][0]).replace("data:image/png;base64,","")
     im = Image.open(BytesIO(base64.b64decode(opimg)))
     st.image(im)
