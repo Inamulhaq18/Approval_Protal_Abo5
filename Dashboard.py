f3de427a02c358b174f27198c8df13a90aab43fb
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-10-19 19:00:31 +0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -10,7 +10,9 @@ from io import BytesIO
 
 
 #initialize the database connection
-conn=psycopg2.connect("postgres://hkmuctkbhmlhsr:59563300aab6c650f8bbc9cc4153df6a42054b71e9be00dda420f40bbbf791b2@ec2-54-76-43-89.eu-west-1.compute.amazonaws.com:5432/dd8a5bspvhrk8c") 
+#postgres://ue0bragorjpsfg:p3401de69df0671d626efa0688fbb4b255afe17a00d95341e8504b5442c3516f3@ec2-52-18-7-194.eu-west-1.compute.amazonaws.com:5432/d1en285kafvdds
+#postgres://hkmuctkbhmlhsr:59563300aab6c650f8bbc9cc4153df6a42054b71e9be00dda420f40bbbf791b2@ec2-54-76-43-89.eu-west-1.compute.amazonaws.com:5432/dd8a5bspvhrk8c
+conn=psycopg2.connect("postgres://ue0bragorjpsfg:p3401de69df0671d626efa0688fbb4b255afe17a00d95341e8504b5442c3516f3@ec2-52-18-7-194.eu-west-1.compute.amazonaws.com:5432/d1en285kafvdds") 
 curr=conn.cursor()
 
 #loading the data
