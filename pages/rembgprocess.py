21b34beb0c071f4d2388bc2394cf2ff9f9603550
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-10-19 11:48:30 +0300

    Update rembgprocess.py

--- pages/rembgprocess.py
+++ pages/rembgprocess.py
@@ -32,7 +32,7 @@ new_img.save(buffer, format="PNG")
 img_b64 = base64.b64encode(buffer.getvalue())
 imgs=img_b64
 imgs=str(imgs).replace("b'","")
-r = requests.post(url='https://hf.space/embed/eugenesiow/remove-bg/+/api/predict/', json={"data": ["data:image/jpeg;base64,"+imgs]})
+r = requests.post(url='https://hf.space/embed/eugenesiow/remove-bg/+/api/predict/', json={"data": ["data:image/jpeg;base64,"+imgs,10,"alpha matting"]})
 st.write(r.json())
 opimg=str(r.json()["data"][0]).replace("data:image/png;base64,","")
 im = Image.open(BytesIO(base64.b64decode(opimg)))
