735b1c1cc0e4cd7d7e0cc920af79da8a0714efbd
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-10-19 19:19:16 +0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -8,6 +8,7 @@ from PIL import Image
 import requests
 from io import BytesIO
 
+st.title("Dashboard")
 
 #initialize the database connection
 #postgres://ue0bragorjpsfg:p3401de69df0671d626efa0688fbb4b255afe17a00d95341e8504b5442c3516f3@ec2-52-18-7-194.eu-west-1.compute.amazonaws.com:5432/d1en285kafvdds
