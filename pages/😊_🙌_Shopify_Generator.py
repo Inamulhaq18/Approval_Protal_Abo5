7f6f926d30ceeadbb0c22e0daa7d17f243a86bb7
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-10-22 11:14:27 +0300

    Update 😊_🙌_Shopify_Generator.py

--- pages/😊_🙌_Shopify_Generator.py
+++ pages/😊_🙌_Shopify_Generator.py
@@ -209,6 +209,7 @@ for index, row in pfa.iterrows():
                urlsdataa=geturlfor(dataa[0],row)#this needs to return url for a 1,2,3,R1
             except:
                st.write(dataa)
+               st.write(row['Product_Name_en'])
             urlsdata.append(urlsdataa)
 
           imagevurl=urlsdata
