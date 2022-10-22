f7d5dc5f394e0ecb23598a0cef0dff441380fc8a
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-10-22 10:36:03 +0300

    Update 🆔Product_ID.py

--- pages/🆔Product_ID.py
+++ pages/🆔Product_ID.py
@@ -15,7 +15,7 @@ import urllib.request
 
 st.header("Hi, Mannar")
 def skip(product_id):
-        conn=psycopg2.connect("postgresql://hkmuctkbhmlhsr:59563300aab6c650f8bbc9cc4153df6a42054b71e9be00dda420f40bbbf791b2@ec2-54-76-43-89.eu-west-1.compute.amazonaws.com:5432/dd8a5bspvhrk8c") 
+        conn=psycopg2.connect("postgres://ue0bragorjpsfg:p3401de69df0671d626efa0688fbb4b255afe17a00d95341e8504b5442c3516f3@ec2-52-18-7-194.eu-west-1.compute.amazonaws.com:5432/d1en285kafvdds") 
         curr=conn.cursor()
         sql_select_queryskip = """UPDATE master_product_table SET "Product_approval_status"= %s WHERE "Product_id" = %s"""
         status=str("8")
