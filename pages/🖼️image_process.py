fdc3bb81f4a0cde5c7961fd34b60c337d1435072
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-10-24 23:18:14 +0300

    Update 🖼️image_process.py

--- pages/🖼️image_process.py
+++ pages/🖼️image_process.py
@@ -44,12 +44,12 @@ pfa=pfa.sort_values(by="Product_id")
 pfa=pfa.drop_duplicates(subset='Product_Name_en', keep="last")
 #Number of items 
 number_of_items=(pfa[pfa["Product_live_status"]==1]).shape[1]
-
+AgGrid(pfa[pfa['Product_id']==1310])
+st.write("RURL")
 pfa.dropna(subset=["Product_Name_en"])
 #pfa=pfa[pfa['Product_Name_en']!=""]
 pfa=pfa[pfa['Product_image_R_url']!=""]
-AgGrid(pfa[pfa['Product_id']==1310])
-st.write("RURL")
+
 pfa=pfa[pfa['Product_image_P_url']==""]
 AgGrid(pfa)
 st.write("2nd")
