820218c9370b6f72fc649c069d9b3da82ca8121f
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-12-17 14:14:41 +0300

    Update backgroundremove.py

--- backgroundremove.py
+++ backgroundremove.py
@@ -7,36 +7,25 @@ import base64
 import requests
 import streamlit as st 
 from rembg import remove
-
+import urllib.request
+from PIL import Image
 
 
 def removebgapi(links):
-    st.write("inside removebgapi")
     st.write(links)
-    st.write("-1")
-    a=base64.b64encode(requests.get(links).content)
-    st.write("-2")
-    imgs=str(a).replace("b'","")
-    st.write("-3")
-    base64_str = imgs
-    st.write("-4")
-    buffer = BytesIO()
-    st.write("-5")
-    imgdata = base64.b64decode(base64_str)
-    st.write("-6")
-    img = Image.open(BytesIO(imgdata))
-    st.write("-7")
-    st.write(img.size)
+    urllib.request.urlretrieve(links,"gfg.png")
+    img = Image.open("gfg.png")
+    st.image(img)
+    st.write("inside removebgapi")
     st.write("-8")
     img.thumbnail((500, 500))
     st.write("-9")
     st.write(img.size)
     st.write("-10")
     st.write(type(img))
-    img.save(buffer, format="PNG")
     st.write("-11")
-    st.write(type(img))
     imot=remove(img)
+    st.image(imot)
     st.write("done:D")
 #     img_b64 = base64.b64encode(buffer.getvalue())
 #     st.write("-12")
